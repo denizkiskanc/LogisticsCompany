75b4602a884a8d850ac823ed948f5ca5e158422d
denizkiskanc <denizkiskancc@gmail.com>
2022-04-16 12:48:49 +0300

    logistics

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: 'src/template.html'
+            template: 'src/index.html'
         })
     ]
 }
