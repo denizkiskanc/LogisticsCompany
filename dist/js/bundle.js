4f0edcc3a0d758bc6f1551ce7e572377dbf97446
denizkiskanc <denizkiskancc@gmail.com>
2022-04-17 18:26:25 +0300

    son değişiklik

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dist_css_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../dist/css/main.scss */ \"./dist/css/main.scss\");\n\r\n\r\nAOS.init({\r\n\r\n    offset:400,\r\n    duration:1000\r\n  });\r\n\r\n\n\n//# sourceURL=webpack://logisticscompany/./src/js/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dist_css_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../dist/css/main.scss */ \"./dist/css/main.scss\");\n\r\n\r\nAOS.init({\r\n\r\n    \r\n    duration:1000\r\n  });\r\n\r\n\n\n//# sourceURL=webpack://logisticscompany/./src/js/index.js?");
 
 /***/ })
 
